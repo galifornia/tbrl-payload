3e092d68da1e7c0b77021d602fdeb8dd9bc435b1
galifornia <martinho87@gmail.com>
2021-04-30 23:55:40 +0200

    [blocks] Modify Content

--- blocks/Content/index.tsx
+++ blocks/Content/index.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import { Block } from 'payload/types';
-import RichText from '../../components/RichText';
-import classes from './index.module.css';
+import React from "react";
+import { Block } from "payload/types";
+import RichText from "../../components/RichText";
+import classes from "./index.module.css";
 
 export type Type = {
-  blockType: 'content'
-  blockName?: string
-  content: unknown
-}
+  blockType: "content";
+  blockName?: string;
+  content: unknown;
+};
 
 export const Content: Block = {
-  slug: 'content',
+  slug: "content",
   labels: {
-    singular: 'Content',
-    plural: 'Content Blocks',
+    singular: "Content",
+    plural: "Content Blocks",
   },
   fields: [
     {
-      name: 'content',
-      type: 'richText',
+      name: "columns",
+      type: "array",
+      minRows: 1,
+      fields: [
+        {
+          type: "row",
+          fields: [
+            {
+              name: "width",
+              label: "Column Width",
+              type: "select",
+              options: [
+                {
+                  label: "One third",
+                  value: "oneThird",
+                },
+                {
+                  label: "Half",
+                  value: "half",
+                },
+                {
+                  label: "Two third",
+                  value: "twoThird",
+                },
+                {
+                  label: "Full Width",
+                  value: "full",
+                },
+              ],
+              admin: {
+                width: "50%",
+              },
+            },
+            {
+              name: "alignment",
+              label: "Alignment",
+              type: "select",
+              options: [
+                {
+                  label: "Left",
+                  value: "left",
+                },
+                {
+                  label: "Center",
+                  value: "center",
+                },
+                {
+                  label: "Right",
+                  value: "right",
+                },
+              ],
+              admin: {
+                width: "50%",
+              },
+            },
+          ],
+        },
+        {
+          name: "content",
+          type: "richText",
+          required: true,
+        },
+      ],
     },
   ],
 };
@@ -28,10 +89,7 @@ export const Component: React.FC<Type> = (props) => {
 
   return (
     <div className={classes.wrap}>
-      <RichText
-        content={content}
-        className={classes.content}
-      />
+      <RichText content={content} className={classes.content} />
     </div>
   );
 };
