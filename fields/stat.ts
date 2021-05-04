3b8b12e9e42593b3e13c065b40b2a617c4a4b68d
galifornia <martinho87@gmail.com>
2021-05-04 19:54:45 +0200

    Add ImageStatCollage block

---
+++ fields/stat.ts
@@ -0,0 +1,13 @@
+import { Field } from "payload/types";
+
+export type StatType = {
+  // TODO: define type & fields
+};
+
+const stat: Field = {
+  name: "stat",
+  type: "group",
+  fields: [],
+};
+
+export default stat;
