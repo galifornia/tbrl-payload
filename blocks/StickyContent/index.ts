485dedcde692701a689ff30ae44d7a2f3c16b1c9
galifornia <martinho87@gmail.com>
2021-05-04 20:06:25 +0200

    Add StickyContent block

---
+++ blocks/StickyContent/index.ts
@@ -0,0 +1,38 @@
+import { Block } from "payload/types";
+
+export type Section = {
+  label: string;
+  description: unknown;
+};
+
+export type Type = {
+  sections: Section[];
+};
+
+const StickyContent: Block = {
+  slug: "sticky-content",
+  labels: {
+    singular: "Sticky Content Block",
+    plural: "Sticky Content Blocks",
+  },
+  fields: [
+    {
+      name: "sections",
+      label: "Sections",
+      type: "array",
+      minRows: 2,
+      maxRows: 8,
+      fields: [
+        { type: "text", name: "label", label: "Label", required: true },
+        {
+          name: "description",
+          label: "Description",
+          type: "textarea",
+          required: true,
+        },
+      ],
+    },
+  ],
+};
+
+export default StickyContent;
