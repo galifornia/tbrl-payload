06e6898ae2cd7c5c5c9d3d5a92218cd296430b53
galifornia <martinho87@gmail.com>
2021-05-04 20:22:38 +0200

    stat: complete fields & type

--- fields/stat.ts
+++ fields/stat.ts
@@ -1,13 +1,27 @@
 import { Field } from "payload/types";
 
 export type StatType = {
-  // TODO: define type & fields
+  statistic: string;
+  description: string;
 };
 
 const stat: Field = {
   name: "stat",
   type: "group",
-  fields: [],
+  fields: [
+    {
+      name: "statistic",
+      label: "Statistic",
+      type: "richText",
+      required: true,
+    },
+    {
+      name: "description",
+      label: "Description",
+      type: "richText",
+      required: true,
+    },
+  ],
 };
 
 export default stat;
