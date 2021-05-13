ecb3c7535487ea134c64dd252724281edbecc0b6
galifornia <martinho87@gmail.com>
2021-05-13 20:16:42 +0200

    Add global styles

--- css/app.ts
+++ css/app.ts
@@ -1,12 +1,40 @@
 import { createUseStyles } from "react-jss";
+import { htmlFontSize, baselinePX, base } from "./base";
+import { h1, h2, h3, h4, h5, body } from "./type";
+import colors from "./colors";
 
 export const useStyles = createUseStyles({
   "@global": {
     "html body": {
       margin: 0,
+      color: colors.gray,
+    },
+    "*": {
+      boxSizing: "border-box",
     },
     html: {
-      fontFamily: "system-ui, Heveltica, Arial, sans-serif",
+      fontFamily: "system-ui, Heveltica Neue, Helvetica, Arial, sans-serif",
+      fontSize: htmlFontSize,
+      lineHeight: `${baselinePX}px`,
+    },
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    p: {
+      ...body,
+    },
+    ol: {
+      padding: `0 0 0 ${base()}`,
+      margin: `0 0 0 ${base()}`,
+    },
+    ul: {
+      padding: `0 0 0 ${base()}`,
+      margin: `0 0 0 ${base()}`,
+    },
+    li: {
+      ...body,
     },
   },
   app: {
