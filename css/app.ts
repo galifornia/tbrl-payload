2923138e96b968ac01afa2c60e7af05602091c7f
galifornia <martinho87@gmail.com>
2021-05-14 13:18:37 +0200

    Finish global css config

--- css/app.ts
+++ css/app.ts
@@ -2,6 +2,7 @@ import { createUseStyles } from "react-jss";
 import { htmlFontSize, baselinePX, base } from "./base";
 import { h1, h2, h3, h4, h5, body } from "./type";
 import colors from "./colors";
+import queries from "./queries";
 
 export const useStyles = createUseStyles({
   "@global": {
@@ -16,6 +17,9 @@ export const useStyles = createUseStyles({
       fontFamily: "system-ui, Heveltica Neue, Helvetica, Arial, sans-serif",
       fontSize: htmlFontSize,
       lineHeight: `${baselinePX}px`,
+      [queries.m]: {
+        fontSize: htmlFontSize * 0.75,
+      },
     },
     h1,
     h2,
